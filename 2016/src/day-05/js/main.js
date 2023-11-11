494841d25051260b99f3a5e401b94e150133c93c
Storm <61149527+yannicfreson@users.noreply.github.com>
2023-11-11 05:44:02 +0100

    2016-05

--- 2016/src/day-05/js/main.js
+++ 2016/src/day-05/js/main.js
@@ -1,6 +1,7 @@
 // Puzzle: https://adventofcode.com/2016/day/5
 
 const fs = require("fs");
+const crypto = require("crypto");
 
 const INPUT = fs
   .readFileSync("../../../../2016/data/day-05.txt", "utf8")
@@ -18,60 +19,40 @@ function main() {
   console.log(`Part 2: ${part2} (took: ${duration}ms)`);
 }
 
-let hashesStartedWithFiveZeros = [];
-
 function partOne(input) {
   let password = "";
   let index = "0";
+  let foundChars = 0;
 
-  while (password.length < 8) {
+  while (foundChars < 8) {
     let hash = md5(input + index);
     if (hash.startsWith("00000")) {
-      hashesStartedWithFiveZeros.push(hash);
       password += hash[5];
-      console.log({ index }, password);
+      foundChars++;
     }
     index++;
   }
-  console.log(hashesStartedWithFiveZeros);
   return password;
 }
 
 function partTwo(input) {
   let password = Array(8).fill(null);
   let index = 0;
-  let hashesStartedWithFiveZerosWorked = false;
-  while (password.includes(null)) {
-    hashesStartedWithFiveZeros.forEach((hash) => {
+  let foundChars = 0;
+  let found = false;
+
+  while (!found) {
+    let hash = md5(input + index);
+
+    if (hash.startsWith("00000")) {
       let position = hash[5];
       if (position.match(/[0-7]/)) {
         position = Number(position);
         if (password[position] === null) {
           password[position] = hash[6];
-          hashesStartedWithFiveZerosWorked = true;
-          console.log(
-            { index },
-            hashesStartedWithFiveZerosWorked,
-            password.join("")
-          );
-          hashesStartedWithFiveZerosWorked = false;
-        }
-      }
-    });
-    if (!hashesStartedWithFiveZerosWorked) {
-      let hash = md5(input + index);
-      if (hash.startsWith("00000")) {
-        let position = hash[5];
-        if (position.match(/[0-7]/)) {
-          position = Number(position);
-          if (password[position] === null) {
-            password[position] = hash[6];
-            console.log(
-              { index },
-              hashesStartedWithFiveZerosWorked,
-              password.join("")
-            );
-            hashesStartedWithFiveZerosWorked = false;
+          foundChars++;
+          if (foundChars === 8) {
+            found = true;
           }
         }
       }
@@ -83,7 +64,6 @@ function partTwo(input) {
 }
 
 function md5(input) {
-  const crypto = require("crypto");
   return crypto.createHash("md5").update(input).digest("hex");
 }
 
