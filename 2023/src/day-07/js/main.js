8af76287b5569bd95fd58b45577c75282ff8262a
Storm <61149527+yannicfreson@users.noreply.github.com>
2023-12-07 21:29:14 +0100

    2023-07

--- 2023/src/day-07/js/main.js
+++ 2023/src/day-07/js/main.js
@@ -1,6 +1,7 @@
 // Puzzle: https://adventofcode.com/2023/day/7
 
 const fs = require("fs");
+const { json } = require("stream/consumers");
 
 const INPUT = fs
   .readFileSync("../../../../2023/data/day-07.txt", "utf8")
@@ -175,7 +176,7 @@ function partOne(input) {
     }
   });
 
-  // Sort hands by value. If same value, go over cardsAsNumbers and compare. sort ascending
+  // Sort hands by value. If the value is the same, go over cardsAsNumbers and compare. sort ascending
   hands.sort((a, b) => {
     if (a.value < b.value) {
       return -1;
@@ -192,22 +193,237 @@ function partOne(input) {
     }
   });
 
-  // assign ranks to all hands
-  hands.forEach((hand, index) => {
-    hand.rank = index + 1;
-  });
-
-  // assign winnings to all hands
-  hands.forEach((hand) => {
-    hand.winnings = hand.rank * hand.bid;
-  });
-
-  // return the sum of all winnings
-  return hands.reduce((acc, hand) => {
-    return acc + hand.winnings;
-  }, 0);
+  // assign ranks, winnings and return the sum of all winnings
+  return (
+    hands.forEach(
+      (hand, index) => (
+        (hand.rank = index + 1), (hand.winnings = hand.rank * hand.bid)
+      )
+    ),
+    hands.reduce((acc, hand) => acc + hand.winnings, 0)
+  );
 }
 
-function partTwo(input) {}
+function partTwo(input) {
+  let hands = input.split("\n").map((hand) => {
+    return {
+      cards: hand
+        .split(" ")[0]
+        .split("")
+        .map((card) => {
+          switch (card) {
+            case "A":
+              return {
+                label: card,
+                value: 13,
+              };
+            case "K":
+              return {
+                label: card,
+                value: 12,
+              };
+            case "Q":
+              return {
+                label: card,
+                value: 11,
+              };
+            case "J":
+              return {
+                label: card,
+                value: 1,
+              };
+            case "T":
+              return {
+                label: card,
+                value: 10,
+              };
+            default:
+              return {
+                label: card,
+                value: parseInt(card),
+              };
+          }
+        }),
+      bid: parseInt(hand.split(" ")[1]),
+      type: null,
+      value: null,
+      rank: null,
+      winnings: null,
+    };
+  });
+
+  hands.forEach((hand) => {
+    // change the card labels to the max card if the card label is a J
+    let mostOccuringCardLabel = findMostOccuringCardLabel(hand.cards);
+    hand.cards.forEach((card) => {
+      if (card.label === "J") {
+        handContainsJoker = true;
+        card.label = mostOccuringCardLabel;
+      }
+    });
+
+    let labels = hand.cards.map((card) => card.label);
+    let uniqueLabels = [...new Set(labels)];
+
+    // Check for five-of-a-kind
+    // where all five cards have the same label: AAAAA
+    if (uniqueLabels.length === 1) {
+      hand.type = "five-of-a-kind";
+      hand.value = types.find((type) => type.name === "five-of-a-kind").value;
+      return;
+    }
+
+    // Check for four-of-a-kind
+    // where four cards have the same label: AAAAB
+    if (uniqueLabels.length === 2) {
+      let fourSameFound = false;
+      uniqueLabels.forEach((label) => {
+        if (labels.filter((l) => l === label).length === 4) {
+          fourSameFound = true;
+        }
+      });
+
+      if (fourSameFound) {
+        hand.type = "four-of-a-kind";
+        hand.value = types.find((type) => type.name === "four-of-a-kind").value;
+        return;
+      }
+    }
+
+    // Check for full-house
+    // where three cards have the same label and the remaining two cards have the same label: AAABB
+    if (uniqueLabels.length === 2) {
+      let threeSameFound = false;
+      let twoSameFound = false;
+      uniqueLabels.forEach((label) => {
+        if (labels.filter((l) => l === label).length === 3) {
+          threeSameFound = true;
+        } else if (labels.filter((l) => l === label).length === 2) {
+          twoSameFound = true;
+        }
+      });
+
+      if (threeSameFound && twoSameFound) {
+        hand.type = "full-house";
+        hand.value = types.find((type) => type.name === "full-house").value;
+        return;
+      }
+    }
+
+    // Check for three-of-a-kind
+    // where three cards have the same label: AAABC
+    if (uniqueLabels.length === 3) {
+      let threeSameFound = false;
+      uniqueLabels.forEach((label) => {
+        if (labels.filter((l) => l === label).length === 3) {
+          threeSameFound = true;
+        }
+      });
+
+      if (threeSameFound) {
+        hand.type = "three-of-a-kind";
+        hand.value = types.find(
+          (type) => type.name === "three-of-a-kind"
+        ).value;
+        return;
+      }
+    }
+
+    // Check for two-pair
+    // where two cards have the same label and two other cards have the same label: AABBC
+    if (uniqueLabels.length === 3) {
+      let twoSameFound = false;
+      let twoSameFoundAagain = false;
+      uniqueLabels.forEach((label) => {
+        if (labels.filter((l) => l === label).length === 2) {
+          if (twoSameFound) {
+            twoSameFoundAagain = true;
+          } else {
+            twoSameFound = true;
+          }
+        }
+      });
+
+      if (twoSameFound && twoSameFoundAagain) {
+        hand.type = "two-pair";
+        hand.value = types.find((type) => type.name === "two-pair").value;
+        return;
+      }
+    }
+
+    // Check for one-pair
+    // where two cards have the same label: AABCD
+    if (uniqueLabels.length === 4) {
+      let twoSameFound = false;
+      uniqueLabels.forEach((label) => {
+        if (labels.filter((l) => l === label).length === 2) {
+          twoSameFound = true;
+        }
+      });
+
+      if (twoSameFound) {
+        hand.type = "one-pair";
+        hand.value = types.find((type) => type.name === "one-pair").value;
+        return;
+      }
+    }
+
+    // Check for high-card
+    // where all cards have different labels: ABCDE
+    if (uniqueLabels.length === 5) {
+      hand.type = "high-card";
+      hand.value = types.find((type) => type.name === "high-card").value;
+      return;
+    }
+  });
+
+  // sort hands by value. If the value is the same, go over cards.value and compare. sort ascending
+  hands.sort((a, b) => {
+    if (a.value < b.value) {
+      return -1;
+    } else if (a.value > b.value) {
+      return 1;
+    } else {
+      for (let i = 0; i < a.cards.length; i++) {
+        if (a.cards[i].value < b.cards[i].value) {
+          return -1;
+        } else if (a.cards[i].value > b.cards[i].value) {
+          return 1;
+        }
+      }
+    }
+  });
+
+  // assign ranks, winnings and return the sum of all winnings
+  return (
+    hands.forEach(
+      (hand, index) => (
+        (hand.rank = index + 1), (hand.winnings = hand.rank * hand.bid)
+      )
+    ),
+    hands.reduce((acc, hand) => acc + hand.winnings, 0)
+  );
+}
+
+function findMostOccuringCardLabel(cards) {
+  let occurences = [];
+  cards.forEach((card) => {
+    if (occurences[card.label]) {
+      occurences[card.label]++;
+    } else {
+      occurences[card.label] = 1;
+    }
+  });
+
+  // sort the occurences object by value, descending
+  let sortedOccurences = Object.keys(occurences).sort((a, b) => {
+    return occurences[b] - occurences[a];
+  });
+
+  // return the first key of the sortedOccurences object, unless it is a J, then return the second key
+  return sortedOccurences[0] === "J"
+    ? sortedOccurences[1]
+    : sortedOccurences[0];
+}
 
 main();
