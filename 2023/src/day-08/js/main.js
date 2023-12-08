c8d44f34ac0a72dc346341d387e64438ad89d8b5
Storm <61149527+yannicfreson@users.noreply.github.com>
2023-12-08 17:12:21 +0100

    2023-08
    SPEEDERS

--- 2023/src/day-08/js/main.js
+++ 2023/src/day-08/js/main.js
@@ -7,53 +7,58 @@ const INPUT = fs
   .trim();
 
 function main() {
-  let now = Date.now();
+  let now = performance.now();
   let part1 = partOne(INPUT);
-  let duration = Date.now() - now;
-  console.log(`Part 1: ${part1} (took: ${duration}ms)`);
+  let duration = performance.now() - now;
+  console.log(`Part 1: ${part1} (took: ${duration.toFixed(2)}ms)`);
 
-  now = Date.now();
+  now = performance.now();
   let part2 = partTwo(INPUT);
-  duration = Date.now() - now;
-  console.log(`Part 2: ${part2} (took: ${duration}ms)`);
+  duration = performance.now() - now;
+  console.log(`Part 2: ${part2} (took: ${duration.toFixed(2)}ms)`);
 }
 
 function partOne(input) {
   let instructions = input.split("\n")[0].split("");
-
+  let lineParts = [];
   let nodes = input
     .split("\n\n")[1]
     .split("\n")
     .map((line) => {
+      lineParts = line.split(" = ");
+      let parts = lineParts[1]
+        .substring(1, lineParts[1].length - 1)
+        .split(", ");
       return {
-        node: line.split(" = ")[0],
+        node: lineParts[0],
         directions: {
-          left: line.split(" = ")[1].slice(1, -1).split(", ")[0],
-          right: line.split(" = ")[1].slice(1, -1).split(", ")[1],
+          left: parts[0],
+          right: parts[1],
         },
       };
     });
 
   let steps = 0;
-
-  let currentNode = nodes.find((line) => line.node === "AAA");
-
   let instructionsRepeatCount = 0;
-  while (currentNode.node !== "ZZZ") {
+  const instructionsLength = instructions.length;
+
+  const nodeMap = nodes.reduce((acc, line) => {
+    acc[line.node] = line;
+    return acc;
+  }, {});
+
+  let currentNode = nodeMap["AAA"];
+
+  while (true) {
     if (
-      instructions[steps - instructions.length * instructionsRepeatCount] ===
-      "L"
+      instructions[steps - instructionsLength * instructionsRepeatCount] === "L"
     ) {
-      currentNode = nodes.find(
-        (line) => line.node === currentNode.directions.left
-      );
+      currentNode = nodeMap[currentNode.directions.left];
     } else {
-      currentNode = nodes.find(
-        (line) => line.node === currentNode.directions.right
-      );
+      currentNode = nodeMap[currentNode.directions.right];
     }
     steps++;
-    if (steps % instructions.length === 0) {
+    if (steps % instructionsLength === 0) {
       instructionsRepeatCount++;
     }
     if (currentNode.node === "ZZZ") {
@@ -66,68 +71,69 @@ function partOne(input) {
 
 function partTwo(input) {
   let instructions = input.split("\n")[0].split("");
-
   let nodes = input
     .split("\n\n")[1]
     .split("\n")
     .map((line) => {
+      let parts = line
+        .split(" = ")[1]
+        .substring(1, line.split(" = ")[1].length - 1)
+        .split(", ");
       return {
         node: line.split(" = ")[0],
         leaves: {
-          left: line.split(" = ")[1].slice(1, -1).split(", ")[0],
-          right: line.split(" = ")[1].slice(1, -1).split(", ")[1],
+          left: parts[0],
+          right: parts[1],
         },
       };
     });
 
   let steps = 0;
+  let instructionsRepeatCount = 0;
+  let instructionsLength = instructions.length;
 
-  // all nodes that end with A
-  let paths = nodes.filter((line) => line.node.endsWith("A"));
-  let stepsTaken = [];
-
-  paths.forEach((path) => {
-    // calculate how many steps it takes to get to a node that ends with Z
-    let instructionsRepeatCount = 0;
-    while (!path.node.endsWith("Z")) {
-      if (
-        instructions[steps - instructions.length * instructionsRepeatCount] ===
-        "L"
-      ) {
-        path = nodes.find((line) => line.node === path.leaves.left);
-      } else {
-        path = nodes.find((line) => line.node === path.leaves.right);
-      }
-      steps++;
-      if (steps % instructions.length === 0) {
-        instructionsRepeatCount++;
-      }
-      if (path.node.endsWith("Z")) {
-        stepsTaken.push(steps);
-        steps = 0;
-      }
-    }
+  let nodeMap = {};
+  nodes.forEach((line) => {
+    nodeMap[line.node] = line;
   });
 
-  return lcm(stepsTaken);
-}
+  let destinationNodes = new Set(
+    nodes.filter((line) => line.node.endsWith("Z")).map((line) => line.node)
+  );
 
-// helper function to calculate least common multiple
-function lcm(arr) {
-  let lcm = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    let GCD = gcd(lcm, arr[i]);
-    lcm = (lcm * arr[i]) / GCD;
+  let stepsTaken = [];
+
+  for (let path of nodes.filter((line) => line.node.endsWith("A"))) {
+    while (true) {
+      if (
+        instructions[steps - instructionsLength * instructionsRepeatCount] ===
+        "L"
+      ) {
+        path = nodeMap[path.leaves.left];
+      } else {
+        path = nodeMap[path.leaves.right];
+      }
+      steps++;
+      if (steps % instructionsLength === 0) {
+        instructionsRepeatCount++;
+      }
+      if (destinationNodes.has(path.node)) {
+        stepsTaken.push(steps);
+        steps = 0;
+        break;
+      }
+    }
   }
-  return lcm;
+
+  return stepsTaken.reduce(lcm);
 }
 
-// helper function to calculate greatest common divisor
 function gcd(a, b) {
-  if (b === 0) {
-    return a;
-  }
-  return gcd(b, a % b);
+  return b === 0 ? a : gcd(b, a % b);
+}
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
 }
 
 main();
