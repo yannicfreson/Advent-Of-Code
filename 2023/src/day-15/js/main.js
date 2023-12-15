89abe809f034f56c7b15ea5c4467ea08ac1ceb9e
Storm <61149527+yannicfreson@users.noreply.github.com>
2023-12-15 11:16:48 +0100

    2023-15

--- 2023/src/day-15/js/main.js
+++ 2023/src/day-15/js/main.js
@@ -19,19 +19,38 @@ function main() {
 }
 
 function partOne(input) {
-  return input.split(",").reduce((acc, string) => acc + holidayAsciiStringHelperAlgorithmAppendix1A(string), 0);
+  return input
+    .split(",")
+    .reduce(
+      (acc, string) =>
+        acc + holidayAsciiStringHelperAlgorithmAppendix1A(string),
+      0
+    );
 }
 
 function partTwo(input) {
-  return calculateFocusingPower(holidayAsciiStringHelperManualArrangementProcedure(input));
+  return calculateFocusingPower(
+    holidayAsciiStringHelperManualArrangementProcedure(input)
+  );
 }
 
 function calculateFocusingPower(array) {
-  return array.reduce((sum, [key, box]) => sum + box.reduce((temp, [_, focalLength], i) => temp + parseInt(focalLength) * (i + 1), 0) * (parseInt(key) + 1), 0)
+  return array.reduce(
+    (sum, [key, box]) =>
+      sum +
+      box.reduce(
+        (temp, [_, focalLength], i) => temp + parseInt(focalLength) * (i + 1),
+        0
+      ) *
+        (parseInt(key) + 1),
+    0
+  );
 }
 
 function holidayAsciiStringHelperAlgorithmAppendix1A(s) {
-  return s.split("").reduce((curr, c) => ((curr + c.charCodeAt(0)) * 17) % 256, 0);
+  return s
+    .split("")
+    .reduce((curr, c) => ((curr + c.charCodeAt(0)) * 17) % 256, 0);
 }
 
 function holidayAsciiStringHelperManualArrangementProcedure(input) {
@@ -43,13 +62,21 @@ function holidayAsciiStringHelperManualArrangementProcedure(input) {
     let operation = str.match(/[-=]/)[0];
     let focalLength = str.match(/\d+/) ? str.match(/\d+/)[0] : 0;
 
-    boxes.has(hash) === false ? boxes.set(hash, []) : null
+    boxes.has(hash) === false ? boxes.set(hash, []) : null;
 
     let box = boxes.get(hash);
 
-    operation === "=" ? box.findIndex((x) => x[0] === label) > -1 ? (box[box.findIndex((x) => x[0] === label)][1] = focalLength) : box.push([label, focalLength]) : box.findIndex((x) => x[0] === label) > -1 && box.splice(box.findIndex((x) => x[0] === label), 1);
+    operation === "="
+      ? box.findIndex((x) => x[0] === label) > -1
+        ? (box[box.findIndex((x) => x[0] === label)][1] = focalLength)
+        : box.push([label, focalLength])
+      : box.findIndex((x) => x[0] === label) > -1 &&
+        box.splice(
+          box.findIndex((x) => x[0] === label),
+          1
+        );
   });
-  
+
   return Array.from(boxes);
 }
 
